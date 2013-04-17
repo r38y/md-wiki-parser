52b8e154b4d192fe816e5095a4c26520b307c37a
Randy Schmidt <me@r38y.com>
2013-04-17 14:55:23 -0400

    TOC and html

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-md-wiki-parser
-==============
+# GOals
+
+1. Parse a Markdown file into sections that can be edited where each
+   section is a heading and stuff under it until the next heading
+2. Sections are everything under a heading
--- md-wiki-parser.rb
+++ md-wiki-parser.rb
@@ -3,11 +3,24 @@
 require 'redcarpet'
 
 def md(text)
-  markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true, space_after_headers: true)
-  html = markdown.render(text)
-  html.gsub!(/^[\w\<][^\n]*\n+/) do |x|
-    x =~ /\n{2}/ ? x : (x.strip!; x << "  \n")
-  end
+  toc_renderer = Redcarpet::Render::HTML_TOC.new
+
+  html_renderer = Redcarpet::Render::HTML.new(
+    hard_wrap: true,
+    with_toc_data: true,
+  )
+  toc_markdown = Redcarpet::Markdown.new(toc_renderer,
+    autolink: true,
+    space_after_headers: true,
+  )
+  html_markdown = Redcarpet::Markdown.new(html_renderer,
+    autolink: true,
+    space_after_headers: true,
+  )
+  output = []
+  output << toc_markdown.render(text)
+  output << html_markdown.render(text)
+  output.join('')
 end
 
 text = File.read('example.md')
@@ -16,5 +29,6 @@ text = File.read('example.md')
 
 html = md(text)
 
-File.open('example.html', 'w') {|f| f.write(html) }
+output_file = File.join(File.dirname(File.expand_path(__FILE__)), 'example.html')
+File.open(output_file, 'w') {|f| f.write(html) }
 
